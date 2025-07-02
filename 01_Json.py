350787ad18626524c66079812abdf875674d6f41
Utkarshnegi2k5 <utkarshnegi05@gmail.com>
2025-07-02 10:56:07 +0530

    python

---
+++ 01_Json.py
@@ -0,0 +1,14 @@
+devops = {
+    "skill":"Devops",
+    "Year":2023,
+    "Tech":{
+        "Cloud":"AWS",
+        "Container":"K8s",
+        "CICD":"Jenkins",
+        "Gitops":[
+            "Gitlab",
+            "ArgoCD",
+            "tekton",
+        ]
+    }
+}
